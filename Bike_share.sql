33a1ec0e201b8b02e25152a5f65963196ee830b9
Abi <zulfahnurabdillah@gmail.com>
2025-09-07 16:49:14 +0700

    Add files via upload

---
+++ Bike_share.sql
@@ -0,0 +1,21 @@
+with bike as (
+SELECT * from bike_share_yr_0
+union all
+SELECT * from bike_share_yr_1)
+
+
+select  
+dteday,
+season,
+a.yr,
+weekday,
+hr,
+rider_type,
+riders,
+price,
+COGS,
+riders*price as revenue,
+riders*price-COGS as profit
+from bike a
+left join cost_table b
+on a.yr = b.yr
